7f762f003c8737d05d90a44e966afe96d9e59b6e
droqen <droqen@gmail.com>
2026-05-10 23:07:01 -0400

    changed name, itch cover, export, etc

--- main/start.gd
+++ main/start.gd
@@ -1,7 +1,7 @@
 extends Node
 
 func _ready() -> void:
-	if OS.has_feature("editor") and false:
+	if OS.has_feature("editor"):
 		Dreamer.debug_mode = true
 		Dreamer.dream(load("res://dreams/06-brightgreenmenu/06-brightgreenmenu_Dream.tres"))
 	else:
